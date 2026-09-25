98fdeb1ff21cb4abea168a40fcca857cf22437ee
alarabystore2008 <marwansammy12@yahoo.com>
2026-09-25 04:21:41 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Firebase configuration for Alarabi Store
+// This file contains the public web-app configuration, not a Google account password.
+export const firebaseConfig = {
+  apiKey: "AIzaSyCc7rrBzGHQSVlhQzMxaC-rPIelAkyeucg",
+  authDomain: "al-araby-store.firebaseapp.com",
+  projectId: "al-araby-store",
+  storageBucket: "al-araby-store.firebasestorage.app",
+  messagingSenderId: "153726873009",
+  appId: "1:153726873009:web:3ef7100eb614dab55de190",
+  measurementId: "G-H6H9XBYTT3"
+};
